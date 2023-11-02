0ee06b9733e4e88aa93451fd4f1567f7767f3ebe
Jayanta <bjayanta.me@gmail.com>
2023-11-02 17:39:09 +0600

    add text, array and object format

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
-function cartesian(...args) {
+function generateCartesian(args) {
     var r = [], 
         max = args.length - 1;
 
@@ -20,11 +20,50 @@ function cartesian(...args) {
     return r;
 }
 
+function cartesian(options, formatOrVariants=[]) {
+    const combinations = generateCartesian(options)
+
+    // Text Format
+    if(formatOrVariants.constructor === String && formatOrVariants === 'plane') {
+        const output = []
+
+        combinations.map(combination => output.push(combination.join('-')))
+
+        return output
+    }
+
+    // Object Format
+    if(formatOrVariants.constructor === Array && formatOrVariants.length > 0) {
+        const option = {}
+
+        combinations.map(combination => {
+            combination.map((data, i) => option[variants[i]] = data)
+        })
+        
+    }
+
+    // Array Format
+    return combinations
+}
+
+
+
 // Variants
+variants = ['size', 'color', 'country', 'grade'];
+
+// Options
 const size = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 const color = ['Red', 'Green', 'Blue', 'Yellow']
 const country = ['BD', 'PK', 'IN']
 const grade = ['A', 'B']
 
-const result = cartesian(size, color, country, grade)
+// Array Format
+const result = cartesian([size, color, country])
+
+// Text Format
+// const result = cartesian([size, color, country], 'plane')
+
+// Object Format
+// const result = cartesian([size, color, country, grade], variants)
+
 console.log(result);
